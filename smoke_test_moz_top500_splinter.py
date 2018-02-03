1d9a2f304bccf13404187b95f0a6aae6037d1cf1
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2018-02-03 15:00:11 +0100

    --wip-- [skip ci]

--- smoke_test_moz_top500_splinter.py
+++ smoke_test_moz_top500_splinter.py
@@ -78,65 +78,74 @@ def generate_combinations():
     return [item for sublist in l for item in sublist][:10]
 
 
-def write_protocol(offer_h2, domain, url, message=None, fs=None, events=None, logs=None):
+def write_protocol(offer_h2, domain, url, fs=None, events=None, logs=None):
     u = 'h2_' if offer_h2 else ''
     u += 'http' if url.startswith('http://') else 'https'
     u += '_www.' if '://www.' in url else '_'
     u += domain
     with open("tmp/{}/{}.txt".format(os.environ['SMOKE_TEST_TIMESTAMP'], u), mode='a') as file:
-        file.write("################################################################################\n".format(domain))
-        file.write("{}\n\n".format(os.environ['SMOKE_TEST_TIMESTAMP']))
-        file.write("offer_h2: {}\n".format(offer_h2))
-        file.write("domain: {}\n".format(domain))
-        file.write("url: {}\n".format(url))
+        file.write("\n################################################################################\n")
+        file.write("{}: offer_h2:{} domain:{} url:{}\n".format(os.environ['SMOKE_TEST_TIMESTAMP'], offer_h2, domain, url))
 
-        if message:
-            file.write("{}\n".format(message))
-
-        file.write("\n\n")
         if fs:
-            file.write("flows in mitmproxy:\n")
+            file.write("\nFlows:\n")
             for fl in fs.keys():
                 file.write("{}\n".format(fl))
         else:
-            file.write("<no flows in mitmproxy>\n")
+            file.write("\n<no flows>\n")
 
         if events:
-            file.write("\n\n")
+            file.write("\nEvents:\n")
             for msg in events:
                 file.write("{: <20} {}".format(msg[0].upper() + ':', msg[1]))
                 file.write("\n")
+        else:
+            file.write("\n<no events>\n")
 
         if logs:
-            file.write("\n\n")
+            file.write("\nLog entries:\n")
             for msg in logs:
                 file.write("{: <6} {}".format(msg.level.upper() + ':', msg.msg))
                 file.write("\n")
+        else:
+            file.write("\n<no log entries>\n")
 
-        file.write("\n\n")
+        file.write("################################################################################\n")
+
+    with open('/tmp/chromedriver-log.log') as f:
+        print(f.read())
+    print('-'*80)
+    with open('/tmp/chromedriver-net-log-output.json') as f:
+        print(f.read())
+    print('-'*80)
+    with open("tmp/{}/{}.txt".format(os.environ['SMOKE_TEST_TIMESTAMP'], u)) as f:
+        print(f.read())
 
 
 class TestSmokeCurl(object):
     @classmethod
     def setup_class(cls):
-        opts = options.Options(
-            listen_port=0,
-            upstream_cert=True,
-            ssl_insecure=True,
-            verbosity='debug',
-            flow_detail=99,
-        )
-        opts.cadir = os.path.expanduser("~/.mitmproxy")
-        tmaster = tservers.TestMaster(opts)
-
-        cls.proxy = proxy = tservers.ProxyThread(tmaster)
-        cls.proxy.start()
+        cls.proxy = {}
+        for offer_h2 in [True, False]:
+            opts = options.Options(
+                listen_port=0,
+                upstream_cert=True,
+                ssl_insecure=True,
+                verbosity='debug',
+                flow_detail=99,
+                http2=offer_h2,
+            )
+            opts.cadir = os.path.expanduser("~/.mitmproxy")
+            tmaster = tservers.TestMaster(opts)
+            cls.proxy[offer_h2] = tservers.ProxyThread(tmaster)
+            cls.proxy[offer_h2].start()
 
         cls.browser = None
 
     @classmethod
     def teardown_class(cls):
-        cls.proxy.shutdown()
+        for proxy in cls.proxy.values():
+            proxy.shutdown()
 
     def teardown_method(self, method):
         if self.browser:
@@ -146,11 +155,11 @@ class TestSmokeCurl(object):
     @flaky(max_runs=3)
     @pytest.mark.parametrize('offer_h2, domain, url', generate_combinations())
     def test_smoke_curl(self, offer_h2, domain, url):
-        self.proxy.tmaster.clear()
-        self.proxy.tmaster.reset([DisableH2C()])
+        self.proxy[offer_h2].tmaster.clear()
+        self.proxy[offer_h2].tmaster.reset([DisableH2C()])
 
         chrome_options = ChromeOptions()
-        chrome_options.add_argument('--proxy-server=http://127.0.0.1:' + str(self.proxy.port))
+        chrome_options.add_argument('--proxy-server=http://127.0.0.1:' + str(self.proxy[offer_h2].port))
         if not offer_h2:
             chrome_options.add_argument('--disable-http2')
         self.browser = Browser('chrome',
@@ -165,34 +174,32 @@ class TestSmokeCurl(object):
         assert self.browser.status_code.is_success()
 
         fs = {}
-        for f in self.proxy.tmaster.state.flows:
+        for f in self.proxy[offer_h2].tmaster.state.flows:
             if f.response:
                 fs[(f.request.http_version, f.request.scheme, f.request.host, f.response.status_code)] = f
 
-        # if not offer_h2:
-        print([k[0] for k in fs.keys()])
-            # assert all([k[0].startswith('HTTP/1') for k in fs.keys()])
+        if not offer_h2:
+            assert all([k[0].startswith('HTTP/1') for k in fs.keys()])
 
         no_failed_flows = len([k for k in fs.keys() if k[3] >= 500]) == 0
         if not no_failed_flows:
-            write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy.tmaster.events, logs=self.proxy.tmaster.logs)
+            write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy[offer_h2].tmaster.events, logs=self.proxy[offer_h2].tmaster.logs)
         assert no_failed_flows
 
         successful_flows = len([k for k in fs.keys() if k[3] == 200]) >= 1
         if not successful_flows:
-            write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy.tmaster.events, logs=self.proxy.tmaster.logs)
+            write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy[offer_h2].tmaster.events, logs=self.proxy[offer_h2].tmaster.logs)
         assert successful_flows
 
         for k, flow in [(k, f) for k, f in fs.items() if k[3] == 200]:
             success = flow.error is None and flow.request and flow.response
             if not success:
-                write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy.tmaster.events, logs=self.proxy.tmaster.logs)
+                write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy[offer_h2].tmaster.events, logs=self.proxy[offer_h2].tmaster.logs)
             assert success
 
-        for msg in self.proxy.tmaster.logs:
+        for msg in self.proxy[offer_h2].tmaster.logs:
             assert 'Traceback' not in msg.msg
 
-        write_protocol(offer_h2, domain, url, fs=fs, events=self.proxy.tmaster.events, logs=self.proxy.tmaster.logs)
 
 if __name__ == '__main__':
     os.environ['SMOKE_TEST_TIMESTAMP'] = time.strftime("%Y%m%d-%H%M")
