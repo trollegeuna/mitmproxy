716baf4cc4191e4f6d7557b9ad88a39642e57570
wvage <wvage@kth.se>
2018-02-27 14:22:40 +0100

    removed old code

--- test/mitmproxy/tools/console/test_common.py
+++ test/mitmproxy/tools/console/test_common.py
@@ -9,7 +9,6 @@ def test_format_flow():
     assert common.format_flow(f, True)
     assert common.format_flow(f, True, hostheader=True)
     assert common.format_flow(f, True, extended=True)
-    
 
 
 def test_format_keyvals():
