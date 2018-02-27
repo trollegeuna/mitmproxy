b1cdbdcd6731937d3fc47befa1a55622761e22a1
Trolle Geuna <trolle.geuna@gmail.com>
2018-02-27 14:37:31 +0100

    Added requirements to tests in options_keypressed
    Added test for m_select NotImplemented error in test_options_keypressed.py
    Added requirements to tests in test_options_keypressed.py

--- test/mitmproxy/tools/console/test_options_keypressed.py
+++ test/mitmproxy/tools/console/test_options_keypressed.py
@@ -1,29 +1,49 @@
+import pytest
 from mitmproxy.tools.console import options
 from mitmproxy.test import taddons
 
-def test_key_pressed_enter():
+"""This document testes the functionallity of the keypress function in
+the Optionlist class created at mitmproxy/tools/console/options.py
+"""
+
+
+def test_esc():
+    """Test if we can send an esc key to keypress. This should if it is accepted
+    give back a answer of None and no errors
+    """
     with taddons.context() as tctx:
         optlist = options.OptionsList(tctx.master)
         optlist.walker.editing = True
-        ans = optlist.keypress((5,5),("enter"))
+        ans = optlist.keypress((5, 5), ("esc"))
         #print("This is a = %s" %(ans))
-        assert ans == "enter"
+        assert ans is None
 
-def test_key_pressed_m_start():
+def test_m_start():
+    """Test if m_start key can be sent. If the function will return m_start"""
     with taddons.context() as tctx:
         optlist = options.OptionsList(tctx.master)
-        ans = optlist.keypress((5,5),("m_start"))
+        ans = optlist.keypress((5, 5), ("m_start"))
         assert ans == "m_start"
 
-def test_key_pressed_m_select():
+def test_m_select_coices():
+    """Test m_select key works with walker.opt.choices set to True"""
     with taddons.context() as tctx:
         optlist = options.OptionsList(tctx.master)
-        ans = optlist.keypress((5,5),("m_select"))
+        optlist.walker.set_focus(1)
+        optlist.walker.focus_obj.opt.choices = True
+        ans = optlist.keypress((5, 5), ("m_select"))
         assert ans == "m_select"
 
-
-def test_key_pressed_m_select():
+def test_m_select_error():
+    """ Test to see if we run keypress with m_select but
+        choices and typespec are none and False.
+        This should generate a NotImplementedError
+    """
     with taddons.context() as tctx:
         optlist = options.OptionsList(tctx.master)
-        ans = optlist.keypress((5,5),("m_select"))
-        assert ans == "m_select"
+        optlist.walker.set_focus(1)
+        optlist.walker.focus_obj.opt.typespec = None
+        optlist.walker.focus_obj.opt.choices = False
+
+        with pytest.raises(NotImplementedError):
+            optlist.keypress((5, 5), ("m_select"))
